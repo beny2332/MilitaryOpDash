ac16717aef2feb2ea2c8769d39cec0628750f940
BinyaminYacovson <bj57577@gmail.com>
2024-10-31 11:04:39 +0200

    app and index.css updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react"
+import { List } from "./components/List"
+import Item from "./models/itemModel"
 
 function App() {
+  const [items, setItems] = useState<Item[]>([])
   return (
     <>
-      <div className="app"></div>
+      <div className="app">
+        <List items={items} setItems={setItems}/>
+      </div>
     </>
   )
 }
